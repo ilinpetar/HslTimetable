d12333336aebb2446a9eb390b570c6eaf9b735f5
Petar Ilin <petar.ilin@epassi.com>
2025-05-14 14:08:54 +0300

    Notification redesign

--- app/src/main/java/com/ddnsgeek/ilinpetar/hsltimetable/MainActivity.java
+++ app/src/main/java/com/ddnsgeek/ilinpetar/hsltimetable/MainActivity.java
@@ -1,10 +1,8 @@
 package com.ddnsgeek.ilinpetar.hsltimetable;
 
-import android.Manifest.permission;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -12,10 +10,7 @@ import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -24,7 +19,6 @@ import androidx.preference.PreferenceManager;
 public class MainActivity extends AppCompatActivity {
 
     private static final String CHANNEL_ID = "HstTimetableNotificationChannel";
-    private static final int NOTIFICATION_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,19 +74,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void run() {
-                hslTimetable.obtainTimetables(findViewById(R.id.timetable), builder);
-                if (ContextCompat.checkSelfPermission(MainActivity.this, permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission.POST_NOTIFICATIONS}, 1);
-                    // T0D0: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                NotificationManagerCompat.from(MainActivity.this).notify(NOTIFICATION_ID, builder.build());
+                hslTimetable.obtainTimetables(MainActivity.this, findViewById(R.id.timetable), builder);
                 handler.postDelayed(this, 10000);
             }
         };
